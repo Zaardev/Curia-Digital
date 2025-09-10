906bc6330e951e5a5d1b3137beacffb5d56233e2
Omogbolahan <ajibaregbolahan@gmail.com>
2025-09-10 13:45:38 +0100

    New Changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,19 @@
-require("dotenv").config();
+eleventyConfig.addCollection("posts", () => {
+  const seen = new Set();
+  return entries.items.map(item => {
+    let slug = item.fields.slug;
 
-module.exports = function(eleventyConfig) {
-  return {
-    dir: { input: "src", output: "_site" }
-  };
-};
+    if (seen.has(slug)) {
+      throw new Error(`Duplicate slug detected: ${slug}`);
+    }
+    seen.add(slug);
+
+    return {
+      title: item.fields.title,
+      date: item.fields.date,
+      slug,
+      body: item.fields.body,
+      permalink: `/blog/${slug}/index.html`
+    };
+  });
+});
